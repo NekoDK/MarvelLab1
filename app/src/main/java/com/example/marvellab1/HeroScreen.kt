b8f45f824da84bc38dd199562df815ce9b647fc1
NekoDK <koldim14kol@gmail.com>
2024-03-17 17:28:32 +0600

    Add HeroScreen

---
+++ app/src/main/java/com/example/marvellab1/HeroScreen.kt
@@ -0,0 +1,75 @@
+package com.example.marvellab1
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import com.example.marvellab1.Hero
+
+@Composable
+fun HeroScreen(onClick: () -> Unit, hero: Hero){
+    Box(modifier = Modifier
+        .fillMaxSize()
+    ){
+        AsyncImage(
+            model = hero.cardImageUrl,
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier =  Modifier.matchParentSize(),
+        )
+        Button(
+            onClick = { onClick() },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .wrapContentSize()
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow_back),
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomStart),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Text(
+                text = stringResource(id = hero.cardText),
+                color = Color.White,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(16.dp,0.dp,0.dp,0.dp)
+            )
+            Text(
+                text = stringResource(id = hero.cardDesc),
+                color = Color.White,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
